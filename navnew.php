1750407ec4eb17216246e8d3457f576e14cc4180
thejaynesh <50999288+thejaynesh@users.noreply.github.com>
2020-11-28 16:21:17 +0530

    Create navnew.php

---
+++ navnew.php
@@ -0,0 +1,49 @@
+<nav id="sidebar">
+            <div id="dismiss">
+                <i class="fas fa-arrow-left"></i>
+            </div>
+
+          <div class="sidebar-header"><a href="homev2.php">
+<img src="img/digi.png" height="100" width="125"></a>
+
+            </div>
+
+            <ul class="list-unstyled components">
+                <p>DIgiTrack</p>
+                <li>
+                        <a href="searchpc.php">Search Computer</a>
+                    </li>
+                <li>
+                    <a href="managemc.php">Manage Machine</a>
+                    
+                </li>
+                <li>
+                    <a href="managedev.php">Manage Device</a>
+                    
+                </li>
+                <li>
+                    <a href="managelab.php">Manage Lab</a>
+                </li>
+                <li>
+                    <a href="managespec.php">Manage Specification</a>
+                </li>
+                <li>
+                    <a href="managemem.php">Manage Members</a>
+                </li>
+                <li>
+                    <a href="regcomp.php">Register Complaint</a>
+                </li>
+                <li>
+                    <a href="reg_req.php">Register Request</a>
+                </li>
+            </ul>
+
+            <!--<ul class="list-unstyled CTAs">
+                <li>
+                    <a href="https://bootstrapious.com/tutorial/files/sidebar.zip" class="download">Download source</a>
+                </li>
+                <li>
+                    <a href="https://bootstrapious.com/p/bootstrap-sidebar" class="article">Back to article</a>
+                </li>
+            </ul>-->
+        </nav>
