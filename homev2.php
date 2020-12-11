aa7cefcd62a8c560a9c5390f497a625a70e9f1df
thejaynesh <50999288+thejaynesh@users.noreply.github.com>
2020-12-11 15:37:56 +0530

    Update homev2.php

--- homev2.php
+++ homev2.php
@@ -50,9 +50,11 @@
                     <button class="btn btn-dark d-inline-block d-lg-none ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <i class="fas fa-align-justify"></i>
                     </button>
-
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="nav navbar-nav ml-auto">   
+                        <ul class="nav navbar-nav ml-auto"> 
+                        <li class="nav-item">
+                                <a class="nav-link" href="logout.php"> Home |</a>
+                            </li>  
                             <li class="nav-item">
                             <a class="nav-link" href="#"><?php
    echo "HI - ".$_SESSION['name']." ".$_SESSION['lname']
