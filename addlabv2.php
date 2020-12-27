cd6831edb898ebf995bdd6269917c472a843955d
thejaynesh <50999288+thejaynesh@users.noreply.github.com>
2020-12-27 21:45:25 +0530

    Update addlabv2.php

--- addlabv2.php
+++ addlabv2.php
@@ -150,7 +150,7 @@ td:hover{
 
         <div class="form-submit">
         
-        <input type="submit" value="Add Machine" name="add" id="Submit" class="Submit">
+        <input type="submit" value="Add Lab" name="add" id="Submit" class="Submit">
         <input type="reset" value="Reset" class="submit" id="reset" name="reset" />
             </div>
         </form>
