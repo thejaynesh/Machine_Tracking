ec24abad4c4210013ba99dcf599201c41c1fcf6b
thejaynesh <50999288+thejaynesh@users.noreply.github.com>
2020-12-27 22:04:53 +0530

    Update confirm_deletev2.php

--- confirm_deletev2.php
+++ confirm_deletev2.php
@@ -145,7 +145,7 @@ td:hover{
     />
     <div class="form-submit">
         
-    <input type="submit" value="Delete" name="add" id="Submit" class="Submit">
+    <input type="submit" value="Delete" name="delete" id="Submit" class="Submit">
     <input type="reset" value="Reset" class="submit" id="reset" name="reset" />
         </div>
     <input type="hidden" name="id" value="<?= $_POST['id'] ?>">
