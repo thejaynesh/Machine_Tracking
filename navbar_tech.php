88b9a55051be09d06e78b42e5d09800de249776e
thejaynesh <50999288+thejaynesh@users.noreply.github.com>
2020-12-27 23:14:47 +0530

    Update navbar_tech.php

--- navbar_tech.php
+++ navbar_tech.php
@@ -1,61 +1,24 @@
+<nav id="sidebar">
+            <div id="dismiss">
+                <i class="fas fa-arrow-left"></i>
+            </div>
 
-            <!-- Sidebar Holder -->
-            <nav id="sidebar">
-                <div class="sidebar-header">
-                    <?php
-                    if (!isset($_SESSION['id']))
-                        echo '<a href="index.php"><h3>Machine Tracking</h3></a>';
-                    else
-                        echo '<a href="home.php"><h3>Machine Tracking</h3></a>';
-                    ?>
-                </div>
+          <div class="sidebar-header"><center><a href="homev2.php">
+<img src="img/digi.png" height="100" width="125"></a></center>
 
-                <ul class="list-unstyled components">
-                    <p>Menu</p>
-                    
-                     <li class="">
-                        <a href="#complaintmenu" data-toggle="collapse" aria-expanded="false">Register a complaint</a>
-                        <ul class="collapse list-unstyled" id="complaintmenu">
-                            <li><a href="complaint_form.php">Complaint for PC</a></li>
-                            <li><a href="device_complaint_form.php">Complaint for Hardware</a></li>
-                        </ul>
-                    </li>
-                    
-                    
-                    <li class="">
-                        <a href="#contactmenu" data-toggle="collapse" aria-expanded="false">Contact</a>
-                        <ul class="collapse list-unstyled" id="contactmenu">
-                            <li><a href="#add_member.php">phadnis.anurag@gmail.com</a></li>
-                            <li><a href="#view_member.php">krishlalwani1@gmail.com</a></li>
+            </div>
 
-                        </ul>
-                    </li>
-                    <?php
-                    if(isset($_SESSION['id']))
-                    {
-                        echo "<hr>";
-                        echo "<li>";
-                            echo '<a href="logout.php">Logout</a>';
-                        echo "</li>";
-                    }
-                    ?>
-                     <hr>
-                    <li class="">
-                        <a href="#developer">Developed By:</a>
-                        <ul class="list-unstyled" id="contactmenu">
-                            <li><a href="#view_member.php">Krish Lalwani</a></li>
-                            <li><a href="#add_member.php">Anurag Phadnis</a></li>
-                            <li><a href="#view_member.php">Husain Attari</a></li> 
-                            <li><a href="#add_member.php">Aaditya Rathour</a></li>
-                        </ul>
-                    </li>
-                <hr>
-                </ul>
-            </nav>
-    <div class="container" id="content">
-    <div class="page-header">
-    <button type="button" id="sidebarCollapse" class="navbar-btn">
-        <span></span>
-        <span></span>
-        <span></span>
-    </button>
+            <ul class="list-unstyled components">
+                <p><a href="homev2.php">Home</a></p>
+                
+            </ul>
+
+            <!--<ul class="list-unstyled CTAs">
+                <li>
+                    <a href="https://bootstrapious.com/tutorial/files/sidebar.zip" class="download">Download source</a>
+                </li>
+                <li>
+                    <a href="https://bootstrapious.com/p/bootstrap-sidebar" class="article">Back to article</a>
+                </li>
+            </ul>-->
+        </nav>
